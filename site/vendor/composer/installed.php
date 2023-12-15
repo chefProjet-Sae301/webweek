d46c6651e6ee0cc134502c453770016707a36765
Thomas Soudan <thomassoudan1314@gmail.com>
2023-12-15 10:10:43 +0100

    structure fichier

---
+++ site/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ec8d882d94b4b9fd6ff3f3c8ab1479321100e4d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ec8d882d94b4b9fd6ff3f3c8ab1479321100e4d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
