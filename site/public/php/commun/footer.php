910fe3a7dab0311f796c731513fb213aea38cc9b
gaillardcamille <gaillard.camille48@gmail.com>
2023-12-15 10:41:54 +0100

    Header + Footer
    ajout de header et footer + include

---
+++ site/public/php/commun/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<article>
+		<a href="https://www.facebook.com/LeTempsDesChimeres/"><img src="commun/facebook.png" height="20px" alt="réseau social Facebook de l'association"></a>
+		<a href="https://www.instagram.com/letempsdeschimeres43/"><img src="commun/instagram.png" height="20px" alt="réseau social Instagram de l'association"></a>
+		<a href="https://discord.gg/yQqnfw9A4X"><img src="commun/discord.png" height="20px" alt="réseau social Discord de l'association"></a>
+		<div class="videlarge"></div>
+		<a href="../index.html">Plan du site</a>
+		<a href="../index.html">Mentions Légales</a>
+	</article>
+</footer>
---
+++ site/public/php/commun/header.php
@@ -0,0 +1,14 @@
+<header>
+
+	<nav>
+		<!-- barre de navigation -->
+		<a href="../index.html"><img src="commun/logo.png" alt="Logo Noël des Chimères" height="60px"></a>
+		<div class="videlarge"></div>
+        <a href="../index.html">Accueil</a>
+        <a href="../index.html">Tournoi</a>
+        <a href="../index.html">Jeux</a>
+        <a href="../index.html">Inscription</a>
+
+	</nav>
+	
+</header>
