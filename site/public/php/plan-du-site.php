f847f69d15532bdede3bc065bac929a3a5937bf7
gaillardcamille <gaillard.camille48@gmail.com>
2023-12-22 15:08:31 +0100

    finit

--- site/public/php/plan-du-site.php
+++ site/public/php/plan-du-site.php
@@ -44,12 +44,16 @@ require('../../vendor/autoload.php');
 
 	<footer id="plan-du-site-footer">
 		<article>
-			<a href="https://www.facebook.com/LeTempsDesChimeres/"><img src="commun/facebook.png" height="20px" alt="réseau social Facebook de l'association"></a>
-			<a href="https://www.instagram.com/letempsdeschimeres43/"><img src="commun/instagram.png" height="20px" alt="réseau social Instagram de l'association"></a>
-			<a href="https://discord.gg/yQqnfw9A4X"><img src="commun/discord.png" height="20px" alt="réseau social Discord de l'association"></a>
+			<span>
+				<a href="https://www.facebook.com/LeTempsDesChimeres/"><img src="commun/facebook.png" height="20px" alt="réseau social Facebook de l'association"></a>
+				<a href="https://www.instagram.com/letempsdeschimeres43/"><img src="commun/instagram.png" height="20px" alt="réseau social Instagram de l'association"></a>
+				<a href="https://discord.gg/yQqnfw9A4X"><img src="commun/discord.png" height="20px" alt="réseau social Discord de l'association"></a>
+			</span>
 			<div class="videlarge"></div>
-			<a href="plan-du-site.php">Plan du site</a>
-			<a href="mentions.php">Mentions Légales</a>
+			<span>
+				<a href="plan-du-site.php">Plan du site</a>
+				<a href="mentions.php">Mentions Légales</a>
+			</span>
 		</article>
 	</footer>
 </body>
